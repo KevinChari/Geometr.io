208db464f0543cca8ad7b5d8abe27a1758eb87ad
Kevin Alexander Chari <rleycastillo_1999@hotmail.com>
2021-08-18 16:15:31 -0500

    Calculamos todos los valores necesarios los que son: Área y perímetro del cuadrado, círculo, triángulo en JS

--- figuras.js
+++ figuras.js
@@ -1,1 +1,64 @@
-console.log("Hello, World!");
+// Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadarado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadarado es: " + areaCuadrado + "cm^2");
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del triángulo miden: " 
+    + ladoTriangulo1 
+    + "cm, " 
+    + ladoTriangulo2 
+    + "cm, " 
+    + baseTriangulo 
+    + "cm"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triángulo es de: " + alturaTriangulo + "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo es: " + areaTriangulo + "cm^2");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del círculo es de: " + radioCirculo + "cm");
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del círculo es de: " + diametroCirculo + "cm");
+
+// PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perímetro del círculo es de: " + perimetroCirculo + "cm");
+
+// Área
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El área del círculo es de: " + areaCirculo + "cm^2");
+
+console.groupEnd();

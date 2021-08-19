0912048a671dffd216d6069aecb7d996b949c594
Kevin Alexander Chari <rleycastillo_1999@hotmail.com>
2021-08-18 20:04:38 -0500

    Interactuando con JavaScript desde HTML, maquetación básica

--- figuras.js
+++ figuras.js
@@ -77,3 +77,20 @@ function areaCirculo(radio) {
 //console.log("El área del círculo es de: " + areaCirculo + "cm^2");
 
 console.groupEnd();
+
+
+// Aquí interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = perimetroCuadrado(value);
+    alert(area);
+}
